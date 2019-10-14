36695f606ecf92010bdd38d22cffee9ebb131593
Sumant <sumantkadam@gmail.com>
2019-10-15 09:00:33 +1100

    Added initial models (#5)
    * Added initial models
    
    * Removed meta dat and added unique attribute to required columns
    
    * Fixed models to add unique constraint and other constraints
    
    * Fixed decimal field and unique att
    
    * Added unique contraint to product

--- ozpricechecker/pricefinderapp/models.py
+++ ozpricechecker/pricefinderapp/models.py
@@ -1,3 +1,127 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Currency(models.Model):
+    name = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    prod_base_url = models.TextField(unique=True)
+    dynamic_page = models.BooleanField(default=False)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    prod_url = models.TextField()
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        unique_together = (('store', 'prod_url'),)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPrice(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    # def __str__(self):
+    #     return self.price
+
+
+class UserProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    threshhold = models.DecimalField(max_digits=12, decimal_places=2)
+
+    class Meta:
+        unique_together = (('product', 'user'),)
+
+
+class ScrapeType(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ScrapeTemplate(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    scrape_type = models.ForeignKey(ScrapeType, on_delete=models.CASCADE)
+    xpath = models.TextField()
+
+    class Meta:
+        unique_together = (('store', 'scrape_type'),)
+
+    def __str__(self):
+        return self.xpath
+
+
+class UserNewsLetter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, unique=True)
+
+    # DEFINED = 'DF'
+    # UNDEFINED = 'UD'
+    # NEWS_LETTER_CHOICES = [
+    #     (DEFINED, 'defined'),
+    #     (UNDEFINED, 'undefined'),
+    # ]
+    # state = models.CharField(
+    #     max_length=2,
+    #     choices=NEWS_LETTER_CHOICES,
+    #     default=UNDEFINED,
+    # )
+
+    class Meta:
+        unique_together = (('user', 'name'),)
+
+    def __str__(self):
+        return self.name
+
+
+class NewsLetterUserProduct(models.Model):
+    user_newsletter = models.ForeignKey(UserNewsLetter, on_delete=models.CASCADE)
+    user_product = models.ForeignKey(UserProduct, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('user_newsletter', 'user_product'),)
+
+
+class NewsLetterTime(models.Model):
+    user_newsletter = models.ForeignKey(UserNewsLetter, on_delete=models.CASCADE)
+    # pylint: disable=invalid-name
+    MONDAY = 'MON'
+    TUESDAY = 'TUE'
+    WEDNESDAY = 'WED'
+    THURSDAY = 'THR'
+    FRIDAY = 'FRI'
+    STATURDAY = 'SAT'
+    SUNDAY = 'SUN'
+    WEEKDAY_CHOICES = [
+        (MONDAY, 'monday'),
+        (TUESDAY, 'tuesday'),
+        (WEDNESDAY, 'wednesday'),
+        (THURSDAY, 'thursday'),
+        (FRIDAY, 'friday'),
+        (STATURDAY, 'staturday'),
+        (SUNDAY, 'sunday'),
+    ]
+    week_day = models.CharField(
+        max_length=3,
+        choices=WEEKDAY_CHOICES,
+        default=MONDAY,
+    )
+    time = models.TimeField()
