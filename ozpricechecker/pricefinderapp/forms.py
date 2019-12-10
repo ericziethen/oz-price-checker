9ad5dc2dff69f86c8dfe2e60dd8550d4f485c50c
ericziethen <48635802+ericziethen@users.noreply.github.com>
2019-12-11 08:37:27 +1100

    Feature/store admin options (#29)
    * Add ezscrape for web functionality
    
    * add store and currency admin
    
    * Pin network dependencies
    
    * misc
    
    * misc
    
    * add unit tests
    
    * fix linting issue

---
+++ ozpricechecker/pricefinderapp/forms.py
@@ -0,0 +1,36 @@
+"""Forms for the App."""
+
+from django.core.validators import URLValidator
+from django.forms import ModelForm, ValidationError
+
+from ezscrape.scraping.scraper import check_url
+
+from .models import Store
+
+
+class StoreCreateForm(ModelForm):
+    """Create form for the Store Model."""
+
+    class Meta:
+        """Define the Form meta data."""
+
+        model = Store
+        fields = ('name', 'prod_base_url', 'currency', 'dynamic_page')
+
+    def clean_prod_base_url(self):
+        """Validate the Store base url."""
+        base_url = self.cleaned_data['prod_base_url']
+
+        # Validate the Url is in the correct Form
+        url_validator = URLValidator(schemes=['http', 'https'])
+        try:
+            url_validator(base_url)
+        except ValidationError as error:
+            raise ValidationError(F'Invalid Url: {error} - Dont forget to include the schema like http or https',
+                                  code='invalid_format')
+
+        # Check if we can reach the url
+        if not check_url(base_url, local_only=False):
+            raise ValidationError(F'Cannot Reach URL', code='cannot_reach_url')
+
+        return base_url
---
+++ ozpricechecker/pricefinderapp/tests/test_forms.py
@@ -0,0 +1,60 @@
+"""Testing forms."""
+
+from unittest import mock
+
+from django.test import TestCase
+
+from pricefinderapp.forms import StoreCreateForm
+from pricefinderapp.models import Currency
+
+
+class TestStoreCreateForm(TestCase):
+    def setUp(self):
+        self.currency = Currency.objects.create(name='AUD')
+
+    @mock.patch('pricefinderapp.forms.check_url')
+    def test_valid_form_created(self, test_patch):
+        test_patch.return_value = True
+        url = 'http://www.url.com'
+        form_data = {
+            'name': 'TestStore',
+            'prod_base_url': url,
+            'currency': self.currency,
+            'dynamic_page': True,
+        }
+
+        form = StoreCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+        self.assertEqual(form.cleaned_data['name'], 'TestStore')
+        self.assertEqual(form.cleaned_data['prod_base_url'], url)
+        self.assertEqual(form.cleaned_data['currency'], self.currency)
+        self.assertEqual(form.cleaned_data['dynamic_page'], True)
+
+    def test_form_created_url_invalid_schema(self):
+        url = 'www.url.com'
+        form_data = {
+            'name': 'TestStore',
+            'prod_base_url': url,
+            'currency': self.currency,
+            'dynamic_page': True,
+        }
+
+        form = StoreCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.has_error('prod_base_url', code='invalid_format'))
+
+    @mock.patch('pricefinderapp.forms.check_url')
+    def test_form_created_url_not_reachable(self, test_patch):
+        test_patch.return_value = False
+        url = 'http://www.url.com'
+        form_data = {
+            'name': 'TestStore',
+            'prod_base_url': url,
+            'currency': self.currency,
+            'dynamic_page': True,
+        }
+
+        form = StoreCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.has_error('prod_base_url', code='cannot_reach_url'))
