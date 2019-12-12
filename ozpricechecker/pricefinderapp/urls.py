a60ea8deab0f1b333289ff83f514c67246204aec
Kadam, Sumant <Sumant.Kadam@dieboldnixdorf.com>
2019-12-12 20:31:20 +1100

    Home page list view

--- ozpricechecker/pricefinderapp/urls.py
+++ ozpricechecker/pricefinderapp/urls.py
@@ -1,9 +1,13 @@
 """App level URL mapping."""
 
 from django.conf.urls import url
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
+from pricefinderapp.views import UserProductListView
+# from . import views
 
 
 urlpatterns = [
-    url('', TemplateView.as_view(template_name='index.html'), name='home'),
+    # url('', TemplateView.as_view(template_name='index.html'), name='home'),
+    # url('', views.index, name='home'),
+    url('', UserProductListView.as_view(), name='user_products'),
 ]
--- ozpricechecker/pricefinderapp/views.py
+++ ozpricechecker/pricefinderapp/views.py
@@ -1,3 +1,19 @@
-# from django.shortcuts import render
+"""App level view."""
 
-# Create your views here.
+# from django.shortcuts import render
+from django.views.generic.list import ListView
+from pricefinderapp.models import UserProduct
+
+
+class UserProductListView(ListView):
+    """User product list view."""
+
+    model = UserProduct
+    paginate_by = 100
+
+
+# def index(request):
+#     """View function for home page of site."""
+#     user_products = UserProduct.objects.all()
+#     print(user_products)
+#     return render(request, 'index.html', {'user_products': user_products})
