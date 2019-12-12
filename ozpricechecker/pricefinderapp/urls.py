b1117697a49fa32c1e4d89c2126725814756b14d
Kadam, Sumant <Sumant.Kadam@dieboldnixdorf.com>
2019-12-12 20:31:20 +1100

    Home page list view

--- ozpricechecker/pricefinderapp/urls.py
+++ ozpricechecker/pricefinderapp/urls.py
@@ -1,5 +1,6 @@
 """App level URL mapping."""
 
+<<<<<<< HEAD
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
@@ -8,4 +9,16 @@ from . import views
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='userproduct_list', permanent=False), name='home'),
     path('userproduct/', views.UserProductListView.as_view(), name='userproduct_list'),
+=======
+from django.conf.urls import url
+# from django.views.generic import TemplateView
+from pricefinderapp.views import UserProductListView
+# from . import views
+
+
+urlpatterns = [
+    # url('', TemplateView.as_view(template_name='index.html'), name='home'),
+    # url('', views.index, name='home'),
+    url('', UserProductListView.as_view(), name='user_products'),
+>>>>>>> Home page list view
 ]
