a66e2dc7b1ccece97d03d15a66e52233106ce24f
Kadam, Sumant <Sumant.Kadam@dieboldnixdorf.com>
2019-12-13 08:24:09 +1100

    Added list view for user products

--- ozpricechecker/pricefinderapp/views.py
+++ ozpricechecker/pricefinderapp/views.py
@@ -1,5 +1,4 @@
 """App level view."""
-<<<<<<< HEAD
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
@@ -16,23 +15,3 @@ class UserProductListView(LoginRequiredMixin, ListView):  # pylint: disable=too-
     def get_queryset(self):
         """Only for current user."""
         return UserProduct.objects.filter(user=self.request.user)
-=======
-
-# from django.shortcuts import render
-from django.views.generic.list import ListView
-from pricefinderapp.models import UserProduct
-
-
-class UserProductListView(ListView):
-    """User product list view."""
-
-    model = UserProduct
-    paginate_by = 100
-
-
-# def index(request):
-#     """View function for home page of site."""
-#     user_products = UserProduct.objects.all()
-#     print(user_products)
-#     return render(request, 'index.html', {'user_products': user_products})
->>>>>>> Home page list view
