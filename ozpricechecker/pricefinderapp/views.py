27ba5e80aded5a5750938db5f5c1f4b38013b326
Kadam, Sumant <Sumant.Kadam@dieboldnixdorf.com>
2019-12-13 13:29:42 +1100

    fixed space for pylint

--- ozpricechecker/pricefinderapp/views.py
+++ ozpricechecker/pricefinderapp/views.py
@@ -4,7 +4,7 @@ from django.views.generic.list import ListView
 from pricefinderapp.models import UserProduct
 
 
-class UserProductListView(ListView): # pylint: disable=too-many-ancestors
+class UserProductListView(ListView):  # pylint: disable=too-many-ancestors
     """User product list view."""
 
     model = UserProduct
