c9481ac5525869ddeff14e0fc1768ece0f4be9e0
Kadam, Sumant <Sumant.Kadam@dieboldnixdorf.com>
2019-12-16 08:39:52 +1100

    Login required changed mixin

--- ozpricechecker/pricefinderapp/views.py
+++ ozpricechecker/pricefinderapp/views.py
@@ -1,12 +1,11 @@
 """App level view."""
 
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from pricefinderapp.models import UserProduct
 
 
-class UserProductListView(ListView):  # pylint: disable=too-many-ancestors
+class UserProductListView(LoginRequiredMixin, ListView):  # pylint: disable=too-many-ancestors
     """User product list view."""
 
     model = UserProduct
@@ -16,8 +15,3 @@ class UserProductListView(ListView):  # pylint: disable=too-many-ancestors
     def get_queryset(self):
         """Only for current user."""
         return UserProduct.objects.filter(user=self.request.user)
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):  # pylint: disable=arguments-differ
-        """Relogin if user not logged in."""
-        return super(UserProductListView, self).dispatch(*args, **kwargs)
