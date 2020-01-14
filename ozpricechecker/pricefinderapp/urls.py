ed538a4965411d21cdd56bf0af1251fe877be715
Sumant <sumant.kadam@gmail.com>
2020-01-14 23:47:14 +1100

    Added update for user product

--- ozpricechecker/pricefinderapp/urls.py
+++ ozpricechecker/pricefinderapp/urls.py
@@ -8,4 +8,7 @@ from . import views
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='userproduct_list', permanent=False), name='home'),
     path('userproduct/', views.UserProductListView.as_view(), name='userproduct_list'),
+    path('adduserproduct/', views.UserProductCreateView.as_view(), name='userproduct_add'),
+    path('update/<int:pk>', views.UserProductUpdateView.as_view(), name='userproduct_update'),
+    path('delete/<int:pk>', views.UserProducttDeleteView.as_view(), name='userproduct_delete'),
 ]
--- ozpricechecker/pricefinderapp/views.py
+++ ozpricechecker/pricefinderapp/views.py
@@ -2,6 +2,8 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from pricefinderapp.models import UserProduct
 
 
@@ -15,3 +17,25 @@ class UserProductListView(LoginRequiredMixin, ListView):  # pylint: disable=too-
     def get_queryset(self):
         """Only for current user."""
         return UserProduct.objects.filter(user=self.request.user)
+
+
+class UserProductCreateView(LoginRequiredMixin, CreateView):
+    model = UserProduct
+    template_name = 'pricefinderapp/userproduct_add.html'
+    fields = ('product', 'threshhold', )
+    success_url = reverse_lazy('userproduct_list')
+
+
+class UserProductUpdateView(LoginRequiredMixin, UpdateView): 
+    model = UserProduct
+    template_name = 'pricefinderapp/userproduct_update.html'
+    fields = ('threshhold', )
+    context_object_name = 'UserProduct'
+    success_url = reverse_lazy('userproduct_list')
+
+    # def get_success_url(self):
+    #     return reverse_lazy('userproduct_list', kwargs={'pk': self.object.id})
+
+
+class UserProducttDeleteView(DeleteView): 
+    model = UserProduct
