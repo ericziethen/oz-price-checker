c5d8bfc38c3ec34de753596e1a7422b25e9132c5
Sumant <sumant.kadam@gmail.com>
2020-01-19 21:26:00 +1100

    added CURD operations

--- ozpricechecker/pricefinderapp/views.py
+++ ozpricechecker/pricefinderapp/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db import IntegrityError
+from django.http import HttpResponse
 from pricefinderapp.models import UserProduct
 
 
@@ -20,22 +22,36 @@ class UserProductListView(LoginRequiredMixin, ListView):  # pylint: disable=too-
 
 
 class UserProductCreateView(LoginRequiredMixin, CreateView):
+    """User product create view."""
+
     model = UserProduct
     template_name = 'pricefinderapp/userproduct_add.html'
     fields = ('product', 'threshhold', )
     success_url = reverse_lazy('userproduct_list')
 
+    def form_valid(self, form):
+        """Validate form data."""
+        form.instance.user = self.request.user
+        try:
+            return super().form_valid(form)
+        except IntegrityError:
+            return HttpResponse("ERROR: User Product already exists!")
 
-class UserProductUpdateView(LoginRequiredMixin, UpdateView): 
+
+class UserProductUpdateView(LoginRequiredMixin, UpdateView):
+    """User product update view."""
+
     model = UserProduct
     template_name = 'pricefinderapp/userproduct_update.html'
     fields = ('threshhold', )
     context_object_name = 'UserProduct'
     success_url = reverse_lazy('userproduct_list')
 
-    # def get_success_url(self):
-    #     return reverse_lazy('userproduct_list', kwargs={'pk': self.object.id})
 
+class UserProducttDeleteView(LoginRequiredMixin, DeleteView):
+    """User product delete view."""
 
-class UserProducttDeleteView(DeleteView): 
     model = UserProduct
+    template_name = 'pricefinderapp/userproduct_delete.html'
+    context_object_name = 'UserProduct'
+    success_url = reverse_lazy('userproduct_list')
