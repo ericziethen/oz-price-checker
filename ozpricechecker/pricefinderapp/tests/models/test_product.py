ea85da9ef8533206518ffcd57abb7883ce5ba1a8
Sumant <sumant.kadam@gmail.com>
2020-04-23 23:13:27 +1000

    Added latest product price and date time

--- ozpricechecker/pricefinderapp/tests/models/test_product.py
+++ ozpricechecker/pricefinderapp/tests/models/test_product.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 
 from parameterized import parameterized
 
-from pricefinderapp.models import Currency, Product, Store
+from pricefinderapp.models import Currency, Product, Store, ProductPrice
+
+import time
 
 
 class SetupTests(TestCase):
@@ -18,6 +20,20 @@ class SetupTests(TestCase):
     def test_str_name(self):
         product = Product.objects.create(store=self.store, prod_url='/1234/Tomatoes', name='Super Tomatoes')
         self.assertEqual(str(product), 'Woolworths - Super Tomatoes')
+    
+    def test_latest_prod_price_date(self):
+        product = Product.objects.create(store=self.store, prod_url='/1234/Tomatoes', name='Super Tomatoes')
+        productPrice1 = ProductPrice.objects.create(product=product, price=10)
+        time.sleep(1)
+        productPrice2 = ProductPrice.objects.create(product=product, price=50)
+        time.sleep(1)
+        productPrice3 = ProductPrice.objects.create(product=product, price=25)
+        self.assertEqual(product.latest_price, productPrice3.price)
+        self.assertEqual(product.date_for_latest_price, productPrice3.date_time)
+
+        productPrice3.delete()
+        self.assertEqual(product.latest_price, productPrice2.price)
+        self.assertEqual(product.date_for_latest_price, productPrice2.date_time)
 
 
 class TestProductAttributes(TestCase):
