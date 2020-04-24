281f0469ed2c9f570955c9a9236b36bc04154a0d
eric <eric@home.com>
2020-04-24 20:09:01 +1000

    Add test coverage to import data command

--- ozpricechecker/pricefinderapp/tests/management/commands/test_import_db_data.py
+++ ozpricechecker/pricefinderapp/tests/management/commands/test_import_db_data.py
@@ -1,12 +1,42 @@
 
 from pathlib import Path
 
+import os
+import pytest
+
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
 
 from pricefinderapp.models import (
     Currency, Product, ScrapeType, ScrapeTemplate, Store
 )
+
+class CommandArgumentTestCase(TestCase):
+
+    def test_no_argument_given(self):
+        with pytest.raises(CommandError):
+            args = []
+            opts = {}
+            call_command('import_db_data', *args, **opts)
+
+    def test_invalid_directory_specified(self):
+        invalid_path = 'this-is-not-a-path'
+        self.assertFalse(os.path.exists(invalid_path))
+
+        with pytest.raises(CommandError):
+            args = [invalid_path]
+            opts = {}
+            call_command('import_db_data', *args, **opts)
+
+    def test_csv_files_not_found(self):
+        path = Path('ozpricechecker') / 'pricefinderapp' / 'tests'
+        self.assertTrue(os.path.exists(path))
+
+        with pytest.raises(CommandError):
+            args = [path]
+            opts = {}
+            call_command('import_db_data', *args, **opts)
 
 
 class CommandsTestCase(TestCase):
