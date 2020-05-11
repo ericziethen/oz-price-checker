b0c2267c99b9ec65bfd1c23c4c5e175aa4ec1d57
eric <eric@home.com>
2020-05-11 19:49:22 +1000

    Alternative way for Custom Filter

--- ozpricechecker/pricefinderapp/filters.py
+++ ozpricechecker/pricefinderapp/filters.py
@@ -5,10 +5,46 @@ from pricefinderapp.models import UserProduct, ProductPrice
 from decimal import Decimal
 
 
+
+
+
+class MyFilter(django_filters.filters.Filter):
+
+    def filter(self, qs, value):
+        print('MyFilter.filter', value, qs)
+        filtered_qs = super().filter(qs, value)
+        print('MyFilter.filter, filtered', value, filtered_qs)
+        return filtered_qs
+
+
+class MyNumberFilter(django_filters.filters.NumberFilter):
+
+    def filter(self, qs, value):
+
+        #print('MyFilter.filter', value, qs)
+        #filtered_qs = super().filter(qs, value)
+        #print('MyFilter.filter, filtered', value, filtered_qs)
+        #return filtered_qs
+
+        if value is not None:
+            wanted_ids = set()
+            for user_product in qs:
+                if user_product.product.latest_price <= value:
+                    wanted_ids.add(user_product.pk)
+            return qs.filter(pk__in=wanted_ids)
+
+        return qs
+# TODO - Generalize the Filter so we can easily use it for different Properties without less code
+# e.g. We can pass extra info to the Init Function
+
+
 class UserProductFilter(django_filters.FilterSet):
     """UserProduct filter."""
 
-    price_max = django_filters.NumberFilter(label='PriceMax')
+    #price_max = django_filters.NumberFilter(label='PriceMax')
+    #price_max = MyFilter(label='PriceMax')
+
+    threshold = MyNumberFilter(label='PriceMax <=')
 
     class Meta:
         """UserProduct filter meta data."""
@@ -17,10 +53,11 @@ class UserProductFilter(django_filters.FilterSet):
         # fields = ('product', 'threshhold')
         fields = {
             'product': ['exact'],
-            'threshhold': ['lte', 'gt'],
+            #'threshhold': ['lte', 'gt'],
             # 'product.latest_price': ['exact'],
         }
 
+    '''
     def __init__(self, *args, **kwargs):
         params = args[0].copy()
 
@@ -43,3 +80,4 @@ class UserProductFilter(django_filters.FilterSet):
             return filtered_set.filter(pk__in=wanted_ids)
 
         return filtered_set
+    '''
